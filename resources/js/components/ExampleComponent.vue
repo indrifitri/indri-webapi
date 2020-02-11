1b9070a5959dac0ea2caeb5c54374b8c5015d11b
indrifitri <begusibrahim000@gmail.com>
2020-02-11 00:04:04 -0800

    init

---
+++ resources/js/components/ExampleComponent.vue
@@ -0,0 +1,87 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Test API</div>
+
+                    <div 
+
+                        v-if="loading ===false"
+                        class="card-body">
+                        Contoh hasil Pemanggilan Endpoint API
+
+                        <p>Response:</p>
+                     <table class="table">
+                        <thead class="thead-dark">
+                            <tr>
+                                <th>Nama</th>
+                                <th>Jk</th>
+                                <th>Dibuat pada</th>
+                            </tr>
+
+
+                        </thead>
+                        <tbody>
+                            <!-- php: foreach item as data -->
+                            <tr v-for="item in data":key="item.id">
+                                <td> {{ item.nama}}</td>
+                                <td> {{item.jk}}</td>
+                                <td> {{item.created_at}}</td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    {{eror}}
+                    </div>
+                    <div v-else class="card-body">loading</div>
+
+
+                    
+                </div>
+            </div>
+        </div>
+        <!-- vueProgressBar-->
+        <vue-progress-bar></vue-progress-bar>
+    </div>
+</template>
+
+<script>
+    export default {
+        data(){
+            return{
+                data:[],
+                error:null,
+                loading:false
+            }
+        },
+        mounted() {//untuk memastikan function yang akan diekses
+            //ekusi telah didownload
+            //function bawaan
+            this.getData()// untuk mengakses web service
+        },
+        methods: {
+            getData(){
+                this.loading =true
+                this.$Progress.start()
+                axios.get('/testapi')//1
+                .then((res)=> {//2
+                    // 
+                    this.data=res.data.data
+                    this.loading =false
+                    this.$Progress.finish()
+
+
+                })
+                .catch((error)=> {//3
+                    // 
+
+                    this.error=error.message
+                    this.loading =false
+                    this.$Progress.fail()
+
+                })
+
+            }
+        }
+    }
+</script>
